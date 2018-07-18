70ddc4b3902f28f57388758a63546099df98a66a
Ubuntu <azureuser@cbc-onebox.ortbzt4jfj1u3gwvfphmjhmpcb.xx.internal.cloudapp.net>
2018-07-18 17:56:27 +0000

    Add ability to send and receive messages and some skeleton code for parent/child fork choice rules

--- casper/protocols/sharded_casper/sharded_casper_view.py
+++ casper/protocols/sharded_casper/sharded_casper_view.py
@@ -2,9 +2,10 @@
 """The sharded casper view module extends a view for sharded casper data structures """
 from casper.abstract_view import AbstractView
 from random import randint
-from casper.protocols.sharded_casper.sharded_casper_constants import Constants
-from casper.protocols.sharded_casper.sharded_casper_constants import BlockConstants
-from casper.protocols.sharded_casper.block import Block
+import random
+from casper.protocols.sharded_casper.sharded_casper_constants import Constants, BlockConstants
+from casper.protocols.sharded_casper.block import Block, ShardMessage
+from casper.utils import get_random_str
 import casper.protocols.sharded_casper.forkchoice as forkchoice
 
 class ShardedCasperView(AbstractView):
@@ -18,20 +19,53 @@ class ShardedCasperView(AbstractView):
 
     def estimate(self, data=None):
         """Returns the current forkchoice in this view"""
-        
+
         sharded_tips = { sid: forkchoice.get_fork_choice(
             sid,
-            self.shard_to_last_finalized_block[sid],
-            self.shard_to_children[sid],
+            self.shard_to_last_finalized_block,
+            self.shard_to_children,
             self.latest_messages) for sid in range(1, Constants.NumberOfShards + 1) }
 
-        shard_to_extend = randint(1, Constants.NumberOfShards)
+            
+        sid = randint(1, Constants.NumberOfShards)
+        block = sharded_tips[sid]
+
+        sent_messages = [item for sublist in block.sent_map.values() for item in sublist]
+        new_sent_map = { sid: [sm for sm in block.sent_map[sid]] for sid in range(1, Constants.NumberOfShards + 1) }
+
+        if ((len(sent_messages) < Constants.PerShardSendLimit) and
+            random.Random().random() < 0.3):
+
+            target_sid = 1 if sid == 2 else 2
+            new_sm = ShardMessage("x-shard-" + get_random_str(), sharded_tips[target_sid], 1000)
+            new_sent_map[target_sid].append(new_sm)
+
+        received_messages = [item for sublist in block.received_map.values() for item in sublist]
+        new_received_map = { sid: [sm for sm in block.received_map[sid]] for sid in range(1, Constants.NumberOfShards + 1) }
+        
+        if ((len(received_messages) < Constants.PerShardReceiveLimit) and
+            random.Random().random() < 0.5):
+
+            source_sid = 1 if sid == 2 else 2
+
+            # We don't have to check for duplicate receives as
+            # PerShardReceiveLimit is one, if it was greater than one
+            # then we'll have to make sure we don't receive the same
+            # message twice
+
+            sent_list = sharded_tips[source_sid].sent_map[sid]
+            if (len(sent_list) == 1):
+                new_rm = ShardMessage(sent_list[0].msg, sent_list[0].base_block, sent_list[0].time_to_live)
+                new_received_map[source_sid].append(new_rm)
+        
         new_block = Block(
-            shard_to_extend,
-            sharded_tips[shard_to_extend],
-            { sid: [] for sid in range(1, Constants.NumberOfShards + 1) },
-            { sid: [] for sid in range(1, Constants.NumberOfShards + 1) })
-        sharded_tips[shard_to_extend] = new_block
+            sid,
+            sharded_tips[sid],
+            new_sent_map,
+            new_received_map)
+        sharded_tips[sid] = new_block
+
+        self._print_new_messages(sid, block, new_block)
 
         return sharded_tips
 
@@ -52,3 +86,40 @@ class ShardedCasperView(AbstractView):
                 children[parent] = set()
 
             children[parent].add(tip)
+
+    def _print_new_messages(self, sid, block, new_block):
+
+        print_separator = False
+        
+        for target_sid in range(1, Constants.NumberOfShards + 1):
+            if sid == target_sid:
+                continue
+
+            sent_list = block.sent_map[target_sid]
+            new_sent_list = new_block.sent_map[target_sid]
+            sent_diff_len = len(new_sent_list) - len(sent_list)
+            
+            if (sent_diff_len > 0):
+                print("Newly sent messages from {} to {}".format(sid, target_sid))
+                print(new_sent_list[-sent_diff_len:])
+                print()
+
+                print_separator = True
+
+        for source_sid in range(1, Constants.NumberOfShards + 1):
+            if (sid == source_sid):
+                continue
+
+            received_list = block.received_map[source_sid]
+            new_received_list = new_block.received_map[source_sid]
+            received_diff_len = len(new_received_list) - len(received_list)
+
+            if (received_diff_len > 0):
+                print("Newly received messages from {} to {}".format(source_sid, sid))
+                print(new_received_list[-received_diff_len:])
+                print()
+
+                print_separator = True
+
+        if (print_separator):
+            print("~~")
